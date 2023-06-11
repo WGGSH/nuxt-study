3fc8c3ddf903b0042687d26be8c36a189f83775e
nao <naokipgt@gmail.com>
2023-06-11 11:28:33 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended", // "plugin:vue/vue3-strongly-recommended"もあります
+    "plugin:prettier/recommended",
+  ],
+  env: {
+    node: true,
+  },
+  rules: {},
+};
