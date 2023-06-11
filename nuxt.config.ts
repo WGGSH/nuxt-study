f6d940d32b72b82724c512ab7072dc10de562f8c
nao <naokipgt@gmail.com>
2023-06-11 11:44:21 +0900

    deply test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   router: {
     base: "/nuxt-study/",
+  },
+  app: {
+    cdnURL: "/nuxt-study",
+  },
+  generate: {
+    dir: "docs",
   },
 });
